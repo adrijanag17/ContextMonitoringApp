56345117d7fb0cf3949b6e6e93809d3cd6c005c0
Adrija <71489040+adrijanag17@users.noreply.github.com>
2023-09-08 00:05:29 -0700

    Add event listener on button click

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button symptomsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        symptomsButton = (Button) findViewById(R.id.symptoms);
+        symptomsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSymptomsActivity();
+            }
+        });
+    }
+
+    public void openSymptomsActivity(){
+        Intent intent = new Intent(this, SymptomsActivity.class);
+        startActivity(intent);
     }
 }
